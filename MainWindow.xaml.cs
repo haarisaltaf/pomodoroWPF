cd1d58a6081a1877ba1c1f64df936958a5650347
haarisaltaf <altafhaaris@gmail.com>
2025-11-29 01:59:23 +0000

    Basic Dispatcher Timer + Placeholder Button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace pomodoroWPF
 {
@@ -16,9 +17,32 @@ namespace pomodoroWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        // define variables in class then utilise in method otherwise hard error
+        DispatcherTimer timer;
+        int interval = 1; // tick = 1 every second
+        int maxInterval = 2400; // 40 mins in seconds
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Init timer with DispatcherTimer function and then add tick method
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(interval);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        private void TimerTick(object? sender, EventArgs e)
+        {
+            if (interval <= maxInterval)
+            {
+                TimerTextBox.Text = interval.ToString();
+            } else
+            {
+                timer.Stop();
+            }
+            interval++;
         }
     }
 }
